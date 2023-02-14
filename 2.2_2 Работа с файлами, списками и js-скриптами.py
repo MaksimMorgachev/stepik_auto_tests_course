897e396b8bdc4bd7b2cb51b55e611e1a1f4111e8
MaksimMorgachev <maksim.morgachev.dev@gmail.com>
2023-02-14 13:25:58 +0300

    Add 2.2_2 Работа с файлами, списками и js-скриптами

---
+++ 2.2_2 Работа с файлами, списками и js-скриптами.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+import time
+import math
+import pyperclip
+
+# Математическая функция для решения задачи на сайте
+def calc(x):
+    return str(math.log(abs(12 * math.sin(int(x)))))
+
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+
+
+try:
+    browser = webdriver.Chrome()
+    link = "http://suninjuly.github.io/redirect_accept.html"
+    browser.get(link)
+    browser.find_element(By.CLASS_NAME, 'btn-primary').click()
+    new_window = browser.window_handles[1]
+    browser.switch_to.window(new_window)
+    x = browser.find_element(By.ID, 'input_value').text
+    result = calc(x)
+    answer = browser.find_element(By.ID, 'answer').send_keys(result)
+    browser.find_element(By.CLASS_NAME, 'btn-primary').click()
+    alert = browser.switch_to.alert
+    alert_text = alert.text
+    addToClipBoard = alert_text.split(': ')[-1]
+    pyperclip.copy(addToClipBoard)
+    # Блок копирования в буфер обмена ответа на правильно решенное задание
+    alert = browser.switch_to.alert
+    alert_text = alert.text
+    addToClipBoard = alert_text.split(': ')[-1]
+    pyperclip.copy(addToClipBoard)
+    print(alert_text)
+finally:
+    # ожидание чтобы визуально оценить результаты прохождения скрипта
+    time.sleep(15)
+    # закрываем браузер после всех манипуляций
+    browser.quit()
